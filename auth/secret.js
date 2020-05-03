1ad15876bfbaba8ec0728b4ccd892c29144e5026
Idong Essien <idong.essien@comcast.net>
2020-05-02 23:41:52 -0400

    CRUD functionality for Todos working

--- auth/secret.js
+++ auth/secret.js
@@ -1,3 +1,3 @@
 // module.exports = { jwtSecret: process.env.JWT_SECRET || "This is the secret" };
 
-module.exports = { secret: process.env.secret || "This is the secret" };
+module.exports = { secret: "This is the secret" };
--- todos/todo-router.js
+++ todos/todo-router.js
@@ -9,8 +9,8 @@ router.get('/todos', restricted, (req, res) => {
         .then(users => {
                 res.json(users)
         })
-        .catch(error => res.status(400).json({ message: 'User does not exist' }))
-})
+        .catch(error => res.status(404).json({ message: 'User does not exist' }))
+});
 
 router.post('/todos', restricted, (req, res) => {
 
@@ -19,5 +19,35 @@ router.post('/todos', restricted, (req, res) => {
     task.user_id = req.decodedToken.subject;
 
     Todos.add(task).then(tasks => res.json(task)).catch(err => res.status(400).json({ message: 'task not added' }))
+});
+
+router.put('/todos/:id', restricted, (req, res) => {
+    
+    Todos.update(req.body, req.params.id)
+        .then(id => {
+            
+            res.status(201).json({ message: id + ' updated successfully' })
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500).json({  message: 'Server error', error })
+        })   
+});
+
+router.delete('/todos/:id', restricted, (req, res) => {
+    const { id } = req.params;
+
+    Todos.remove(id)
+        .then(deleted => {
+            if(deleted) {
+                res.status(200).json({ message: 'Task successfully removed' })
+            } else {
+                res.status(404).json({ message: 'Could not find task' })
+            }
+        })
+        .catch(() => {
+            res.status(500).json({ message: 'There was an error deleting task' })
+        })
 })
+
 module.exports = router;
