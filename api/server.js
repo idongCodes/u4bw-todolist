8f8688814b6b96c767c3d012b2f9a406a2ed63f9
Idong Essien <idong.essien@comcast.net>
2020-05-02 21:07:18 -0400

    changed secret syntax in secret.js

--- api/server.js
+++ api/server.js
@@ -15,37 +15,4 @@ server.use('/api/auth', authRouter);
 server.use('/api/users', userRouter);
 server.use('/api/todos', todoRoutes);
 
-
-
-// server.use('/', (req, res) => res.send('ToDo API up and running!'));
-
-
-
-// server.get('/todos', (req, res) => {
-//     const sortField = req.query.sortby || 'id';
-
-//     const response = todos.sort((a, b) => (a[sortField] < b[sortField] ? -1 : 1));
-
-//     res.status(200).json(response);
-// });
-
-// server.post('/todos', (req, res) => {
-//     const todo = req.body;
-//     todo.id = nextId++;
-
-//     todos.push(todo);
-
-//     res.status(201).json(todos);
-// });
-
-// server.put('/todos/:id', (req, res) => {
-   
-// });
-
-// server.delete('/todos/:id', (req, res) => {
-//     const { id } = req.params;
-
-//     res.status(200).json({ url: `/todos/${ id }`, operation: `DELETE for todo with id ${ id }`});
-// });
-
 module.exports = server;
